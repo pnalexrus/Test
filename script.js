a30109821bb5aa3343ebbc8cf9dce918cf1b1c52
pnalexrus <pnalexrus@gmail.com>
2023-10-10 16:02:18 +0300

    10.10.2023 16.02

--- script.js
+++ script.js
@@ -256,37 +256,86 @@ const smallNewsTemplate = document.getElementById('small-article-item')
 const mainNewsContainer = document.querySelector('.articles__big-column')
 const smallNewsContainer = document.querySelector('.articles__small-column')
 
-mainNews.forEach((item) => {
-	const element = mainNewsTemplate.content.cloneNode(true)
-	const category = data.categories.find(
+const createMainItem = (item) => {
+	const categoryData = data.categories.find(
 		(categoryItem) => categoryItem.id === item.category_id
 	).name
-	const source = data.sources.find(
+	const sourceData = data.sources.find(
 		(sourceItem) => sourceItem.id === item.source_id
 	).name
 
-	element.querySelector('.main-article__title').textContent = item.title
-	element.querySelector('.main-article__image').src = item.image
-	element.querySelector('.main-article__category').textContent = category
-	element.querySelector('.main-article__text').textContent = item.description
-	element.querySelector('.main-article__source').textContent = source.name
+	const article = document.createElement('article')
+	const imageContainer = document.createElement('div')
+	const image = document.createElement('img')
+	const content = document.createElement('div')
+	const category = document.createElement('span')
+	const title = document.createElement('h2')
+	const text = document.createElement('p')
+	const source = document.createElement('span')
 
-	mainNewsContainer.appendChild(element)
-})
+	article.classList.add('main-article')
+	imageContainer.classList.add('main-article__image-container')
+	image.classList.add('main-article__image')
+	content.classList.add('main-article__content')
+	category.classList.add('article-category', 'main-article__category')
+	title.classList.add('main-article__title')
+	text.classList.add('main-article__text')
+	source.classList.add('article-source', 'main-article__source')
 
-smallNews.forEach((item) => {
-	const element = smallNewsTemplate.content.cloneNode(true)
-	const source = data.sources.find(
+	title.textContent = item.title
+	image.src = item.image
+	category.textContent = categoryData.name
+	text.textContent = item.description
+	source.textContent = sourceData.name
+
+	imageContainer.appendChild(image)
+	article.appendChild(imageContainer)
+	content.appendChild(category)
+	content.appendChild(title)
+	content.appendChild(text)
+	content.appendChild(source)
+	article.appendChild(content)
+
+	return article
+}
+
+const createSmallItem = (item) => {
+	const sourceData = data.sources.find(
 		(sourceItem) => sourceItem.id === item.source_id
 	).name
-	const date = new Date(item.date).toLocaleDateString('ru-RU', {
+	const dateData = new Date(item.date).toLocaleDateString('ru-RU', {
 		month: 'long',
 		day: 'numeric',
 	})
 
-	element.querySelector('.small-article__title').textContent = item.title
-	element.querySelector('.small-article__source').textContent = source.name
-	element.querySelector('.small-article__date').textContent = date
+	const article = document.createElement('article')
+	const title = document.createElement('h2')
+	const caption = document.createElement('p')
+	const date = document.createElement('span')
+	const source = document.createElement('span')
 
-	smallNewsContainer.appendChild(element)
+	article.classList.add('small-article')
+	title.classList.add('small-article__title')
+	caption.classList.add('small-article__caption')
+	date.classList.add('article-date', 'small-article__date')
+	source.classList.add('article-source', 'small-article__source')
+
+	title.textContent = item.title
+	source.textContent = sourceData.name
+	date.textContent = dateData
+
+	article.appendChild(title)
+	article.appendChild(caption)
+	caption.appendChild(date)
+	caption.appendChild(source)
+
+	return article
+}
+
+mainNews.forEach((item) => {
+	mainNewsContainer.appendChild(createMainItem(item))
+})
+
+smallNews.forEach((item) => {
+	smallNewsContainer.appendChild(createSmallItem(item))
 })
